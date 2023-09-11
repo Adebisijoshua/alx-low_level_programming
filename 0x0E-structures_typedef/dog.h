1443435007b7b0e1fa046a93d9a007553077aa57
Adebisi <adekunle8k@gmail.com>
2023-09-11 15:51:26 -0700

    I am tired, where I am getting it it wrong

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,24 +1,28 @@
-#ifndef _DOG_H
-#define _DOG_H
+#ifndef _HEADER_
+#define _HEADER_
+
 /**
- * struct dog - a struct that create a dog info
- *
- * @name: the name of the dog
- * @age: the age of the dog
- * @owner: the owner of the dog
- *
- * Description: first struct with alx
- */
-
-typedef struct dog
+  * struct dog - Dog attributes
+  * @name: The name of the dog.
+  * @age: The age of the dog.
+  * @owner: The owner of the dog.
+  *
+  * Description: The attributes of a dog.
+  */
+struct dog
 {
-char *name;
-float age;
-char *owner;
+	char *name;
+	float age;
+	char *owner;
+};
 
+/**
+ * dog_t - Typedef for dog structure
+ */
+typedef struct dog dog_t;
 
-
-};dog_t
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 #endif
