4cb65106be5b5f6ead303d5d25bd7c27a04ef035
Your Name <Adekunle8k@gmail.com>
2023-09-25 23:07:35 -0700

    It is annoying

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,24 +1,21 @@
-#include <stddef.h> /* Include for size_t definition */
-#include <stdio.h>
 #include "lists.h"
 
 /**
- * print_listint - Prints all the elements of a listint_t list.
- * @h: A pointer to the head of the linked list.
+ * print_listint - prints all the elements of a linked list
+ * @h: linked list of type listint_t to print
  *
- * Return: The number of nodes in the list.
+ * Return: number of nodes
  */
 size_t print_listint(const listint_t *h)
 {
-    size_t node_count = 0;
+    size_t num = 0;
 
-    while (h != NULL)
+    while (h)
     {
         printf("%d\n", h->n);
+        num++;
         h = h->next;
-        node_count++;
     }
 
-    return (node_count);
+    return (num);
 }
-
