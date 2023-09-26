3dbf4bb80c4cf089bee9effaf6ce583cee542568
Your Name <Adekunle8k@gmail.com>
2023-09-25 23:40:31 -0700

    I commit this several times before the betty gotcoorected

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,21 +1,21 @@
 #include "lists.h"
 
 /**
- * listint_len - Calculates the number of elements in a listint_t list.
- * @h: A pointer to the head of the linked list.
- *
- * Return: The number of elements (nodes) in the list.
- */
+* listint_len - Calculates the number of elements in a listint_t list.
+* @h: A pointer to the head of the linked list.
+*
+* Return: The number of elements (nodes) in the list.
+*/
 size_t listint_len(const listint_t *h)
 {
-    size_t count = 0;
+size_t count = 0;
 
-    while (h != NULL)
-    {
-        count++;
-        h = h->next;
-    }
+while (h != NULL)
+{
+count++;
+h = h->next;
+}
 
-    return (count);
+return (count);
 }
 
