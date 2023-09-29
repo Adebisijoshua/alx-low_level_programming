89c8bd191e528a1164aaa6bc8522967fd663ef18
Your Name <Adekunle8k@gmail.com>
2023-09-29 05:57:04 -0700

    Ade is my name

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -3,7 +3,7 @@
 /**
  * get_bit - Gets the value of a bit at a given index.
  * @n: The number to extract the bit from.
- * @index: The index of the bit to get, where index starts from 0.
+ * @index: The index of the bit to get (starting from 0).
  *
  * Return: The value of the bit at the specified index, or -1 if an error occurs.
  */
@@ -11,11 +11,9 @@ int get_bit(unsigned long int n, unsigned int index)
 {
 	int bit_value;
 
-	/* Check if the index is out of bounds. */
 	if (index >= sizeof(unsigned long int) * 8)
 		return (-1); /* Error: Index out of bounds. */
 
-	/* Shift the number to the right by the index and mask it with 1 to get the bit value. */
 	bit_value = (n >> index) & 1;
 
 	return (bit_value);
