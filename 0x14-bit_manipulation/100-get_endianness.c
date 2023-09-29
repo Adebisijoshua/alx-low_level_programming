87cf4129cf59941ae6c4659d3b905676e49c8619
Your Name <Adekunle8k@gmail.com>
2023-09-29 06:33:49 -0700

    betty error again

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,40 +1,19 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
- * check_endianness - Checks the endianness of the system.
- *
- * Return: 1 if the system is little-endian, 0 if it's big-endian.
- */
-int check_endianness(void)
+* get_endianness - checks the endianness
+*
+* Return: 0 if big endian, 1 if little endian
+*/
+int get_endianness(void)
 {
-	int x = 1;
-	char *c = (char *)&x;
+unsigned int x = 1;
+char *c = (char *)&x;
 
-	if (*c == 1)
-		return (1); /* Little-endian */
+if (*c == 1)
 
-	return (0); /* Big-endian */
-}
+return (1); /* Little-endian */
 
-/**
- * main - Entry point of the program.
- *
- * Description: This function checks the endianness of the system and prints
- * whether it's little-endian or big-endian.
- *
- * Return: Always 0 (Success)
- */
-int main(void)
-{
-	if (check_endianness())
-	{
-		printf("This system is little-endian.\n");
-	}
-	else
-	{
-		printf("This system is big-endian.\n");
-	}
-
-	return (0);
+return (0); /* Big-endian */
 }
 
