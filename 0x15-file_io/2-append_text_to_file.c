1496ade16dc06fa159bd7966dabec40daeb781c2
Your Name <Adekunle8k@gmail.com>
2023-10-02 06:27:06 -0700

    This is the second time I will be coming it

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,53 +1,39 @@
 #include "main.h"
 
 /**
- * _strlen - pack together the  length of  NULL-terminated string
- * @str: the string to measure
+ * append_text_to_file - create a file with specific permission
+ * @filename: Input the name of the file
+ * @text_content:This will contain the file content
  *
- * Return:length of str, or -1 if str will be NULL
- */
-ssize_t _strlen(const char *str)
-{
-	ssize_t len = 0;
-
-	if (!str)
-		return (-1);
-
-	while (*str++)
-		++len;
-
-	return (len);
-}
-
-/**
- * append_text_to_file - to the end of a file
- * @filename: file name to  the file to append to
- * @text_content: name of the file that will be comprehend to the data
+ * Return: 1 if the file is succesful -1 if the file is not successful
  *
- * Return: if it is not fails return 1. incase it fails, return -1.
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	ssize_t a_written = 0;
-
 	int fd;
+	int nletters;
+	int rwr;
 
 	if (!filename)
 		return (-1);
 
 	fd = open(filename, O_WRONLY | O_APPEND);
 
-	if (fd < 0)
-
+	if (fd == -1)
 		return (-1);
 
 	if (text_content)
+	{
+		for (nletters = 0; text_content[nletters]; nletters++)
+			;
 
-		a_written = write(fd, text_content, _strlen(text_content));
+		rwr = write(fd, text_content, nletters);
+
+		if (rwr == -1)
+			return (-1);
+	}
 
 	close(fd);
 
-	if (a_written < 0)
-		return (-1);
 	return (1);
 }
