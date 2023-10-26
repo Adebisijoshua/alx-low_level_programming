218a2d9478b029bcdf03a03e6a6af1139557b5d2
Adebisijoshua <Adekunle8k@gmail.com>
2023-10-25 18:02:40 -0700

    I am redoing this for month 2

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,24 +1,99 @@
+/*
+ *Name of File: 102-free_listint_safe.c
+ *Writer:Adekunle Joshua
+ */
+
 #include "lists.h"
 
+size_t looped_listint_count(listint_t *head);
+size_t free_listint_safe(listint_t **h);
+
 /**
- * free_listint2 - Frees a listint_t list and sets the head to NULL.
- * @head: A pointer to a pointer to the head of the linked list.
+ * looped_listint_count - take d value of special nodes
+ *                      in the listint_t linked list.
+ * @head: It is a pointer to head of listint_t.
+ *
+ * Return: If the list occur as looped - 0.
+ *         If it is not - the no of Special nodes in the list.
  */
-void free_listint2(listint_t **head)
+size_t looped_listint_count(listint_t *head)
 {
-    listint_t *current, *temp;
+	listint_t *tortoise, *hare;
+	size_t nodes = 1;
 
-    if (head == NULL)
-        return;
+	if (head == NULL || head->next == NULL)
+		return (0);
 
-    current = *head;
-    while (current != NULL)
-    {
-        temp = current;
-        current = current->next;
-        free(temp);
-    }
+	tortoise = head->next;
+	hare = (head->next)->next;
 
-    *head = NULL;
+	while (hare)
+	{
+		if (tortoise == hare)
+		{
+			tortoise = head;
+			while (tortoise != hare)
+			{
+				nodes++;
+				tortoise = tortoise->next;
+				hare = hare->next;
+			}
+
+			tortoise = tortoise->next;
+			while (tortoise != hare)
+			{
+				nodes++;
+				tortoise = tortoise->next;
+			}
+
+			return (nodes);
+		}
+
+		tortoise = tortoise->next;
+		hare = (hare->next)->next;
+	}
+
+	return (0);
 }
 
+/**
+ * free_listint_safe - Allows list safely
+ * @h: A pointer to the address of list
+ *
+ * Return: The size of the list to be moved
+ *
+ * Description: The funct will be set to the null
+ */
+size_t free_listint_safe(listint_t **h)
+{
+	listint_t *tmp;
+	size_t nodes, index;
+
+	nodes = looped_listint_count(*h);
+
+	if (nodes == 0)
+	{
+		for (; h != NULL && *h != NULL; nodes++)
+		{
+			tmp = (*h)->next;
+			free(*h);
+			*h = tmp;
+		}
+	}
+
+	else
+	{
+		for (index = 0; index < nodes; index++)
+		{
+			tmp = (*h)->next;
+			free(*h);
+			*h = tmp;
+		}
+
+		*h = NULL;
+	}
+
+	h = NULL;
+
+	return (nodes);
+}
