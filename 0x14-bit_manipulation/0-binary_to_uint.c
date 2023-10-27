5a150e3b0552645fd1e644f73ea1a7da87a9aa88
Adebisijoshua <Adekunle8k@gmail.com>
2023-10-27 11:37:07 -0700

    I amtrying to look for my erro

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * binary_to_uint - It will change a binary number to an unsigned int.
- * @b: pointing to a string containing a binary number
+ * binary_to_uint - It will change a binary num to an unsigned int.
+ * @b: pointing to a string having a binary number
  *
- * Return: unsigned int, or 0 if not success
+ * Return: unsigned int, or 0 if it is not success
  */
 unsigned int binary_to_uint(const char *b)
 {
