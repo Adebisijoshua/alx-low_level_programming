16bed5b544324a68f20fcc75a237f776ad658764
Adebisijoshua <Adekunle8k@gmail.com>
2023-10-30 11:54:30 -0700

    I just fed up

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,19 +1,14 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef __MAIN_H__
+#define __MAIN_H__
 
-#include <fcntl.h>
-#include <stdlib.h>
 #include <sys/stat.h>
 #include <unistd.h>
-#include <sys/types.h>
+#include <stdlib.h>
 #include <fcntl.h>
-#include <stdio.h>
+#include <sys/types.h>
 
-int create_file(const char *filename, char *text_content);
-int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
-int _strlen(char *str);
+int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
-int int create_file(const char *filename, char *text_content);
 
 #endif
